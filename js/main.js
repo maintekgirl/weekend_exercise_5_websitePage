77c2b275e4c3596b22a988e670830d137b16fe0c
TatianaKedrova <127103022+TatianaKedrova@users.noreply.github.com>
2023-04-08 21:40:51 +0200

    Function that change button color VAR2

--- js/main.js
+++ js/main.js
@@ -2,12 +2,11 @@ function alertFunction () {
 alert("Hi! You changed button color");
 }
 
+function changeElementColor () {
+    document.getElementById("btn").style.backgroundColor = "#ff0d7e";
+}
 
-/*function changeColor(element, color) {
-    element.style.color = color;
-  }*/
 
+ /* let btnArticle = document.querySelector("#btn");
 
-  let btnArticle = document.querySelector("#btn");
-
-  btnArticle.addEventListener("click", () => btnArticle.style.backgroundColor="#ff0d7e")
+  btnArticle.addEventListener("click", () => btnArticle.style.backgroundColor="#ff0d7e")*/
