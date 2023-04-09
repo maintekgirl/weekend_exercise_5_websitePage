df79fe76f1a72f9eaf948e3a5a1926b7c59355ac
TatianaKedrova <127103022+TatianaKedrova@users.noreply.github.com>
2023-04-09 14:48:18 +0200

    CLean details

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ function alertFunction () {
 alert("Hi! You changed button color");
 }
 
+
+
 function changeElementColor () {
     document.getElementById("btn").style.backgroundColor = "#ff0d7e";
 }
